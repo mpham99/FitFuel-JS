fd08f8a1863da5fd3d6c851651c98f7d20dedea2
Minh Duc Pham <minhduc99@outlook.com>
2025-03-20 23:43:44 -0400

    Add toggle to use REST API for development purpose

--- constant.js
+++ constant.js
@@ -1,5 +1,7 @@
-// const BASE_URL = 'http://localhost:8080'
-const BASE_URL = 'http://fitfuel-a8hycfhsdxgqb0db.canadacentral-01.azurewebsites.net:80'
+const BASE_URL = 'http://localhost:8080'
+// const BASE_URL = 'http://fitfuel-a8hycfhsdxgqb0db.canadacentral-01.azurewebsites.net:80'
+
+const USE_REST = false; //set to false for development
 
 const LOGIN_EP = '/auth/login'
 const REFRESH_EP = '/auth/refresh'
@@ -7,5 +9,6 @@ const REFRESH_EP = '/auth/refresh'
 module.exports = {
     BASE_URL,
     LOGIN_EP,
-    REFRESH_EP
+    REFRESH_EP,
+    USE_REST
 };
--- controllers/login.js
+++ controllers/login.js
@@ -1,4 +1,4 @@
-const {BASE_URL, LOGIN_EP} = require("../constant");
+const {BASE_URL, LOGIN_EP, USE_REST} = require("../constant");
 
 const express = require("express");
 const axios = require("axios");
@@ -10,26 +10,33 @@ router.get("/", async function(req, res) {
 });
 
 router.post("/", async function(req, res) {
-    try {
-        const { username, password } = req.body;
+    if (USE_REST === true) {
+        try {
+            const { username, password } = req.body;
 
-        // Make authentication API call
-        const response = await axios.post(BASE_URL + LOGIN_EP, {
-            username,
-            password
-        });
+            // Make authentication API call
+            const response = await axios.post(BASE_URL + LOGIN_EP, {
+                username,
+                password
+            });
 
-        if (response.data && response.data.accessToken) {
-            req.session.accessToken = response.data.accessToken;
-            res.redirect("/home");  // Redirect to home page upon success
-        } else {
-            req.TPL.error = "Invalid username or password.";
-            res.render("login_page", req.TPL);  // Reload login page with error
+            if (response.data && response.data.accessToken) {
+                req.session.accessToken = response.data.accessToken;
+                req.session.username = username;
+                console.log(req.session);
+                res.redirect("/home");  // Redirect to home page upon success
+            } else {
+                req.TPL.error = "Invalid username or password.";
+                res.render("login_page", req.TPL);  // Reload login page with error
+            }
+        } catch (error) {
+            console.error("Login error:", error);
+            req.TPL.error = "Authentication failed. Please try again.";
+            res.render("login_page", req.TPL);
         }
-    } catch (error) {
-        console.error("Login error:", error);
-        req.TPL.error = "Authentication failed. Please try again.";
-        res.render("login_page", req.TPL);
+    } else {
+        req.session.username = 'test3';
+        res.redirect("/home");
     }
 });
 
